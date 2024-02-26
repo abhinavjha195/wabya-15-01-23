00d42f3a9c9800b0c2b00a60abc319152f0cf549
abhinavjha195 <abhinav.jha195@gmail.com>
2024-02-26 19:41:22 +0530

    changes-26-02-24

--- src/pages/api/checkout_sessions.js
+++ src/pages/api/checkout_sessions.js
@@ -8,31 +8,57 @@ export default async function handler(req, res) {
       const { doc_id } = req.body; // Get price from the request body 
       const { plan_id } = req.body; // Get price from the request body 
       const { client_id } = req.body; // Get price from the request body 		
-      const session = await stripe.checkout.sessions.create({
-		// billing_address_collection: "required",  
+    //   const session = await stripe.checkout.sessions.create({
+		// // billing_address_collection: "required",  
    
-        payment_method_types: ["card"],    
-        line_items: [
-          {
-            price_data: {
-              currency: "usd",  			  
-              product_data: {
-                name: "sample item",   
-              },
-              unit_amount: price *100,	
+    //     payment_method_types: ["card"],    
+    //     line_items: [
+    //       {
+    //         price_data: {
+    //           currency: "usd",  			  
+    //           product_data: {
+    //             name: "sample item",   
+    //           },
+    //           unit_amount: price *100,	
+    //         },
+    //         quantity: 1,
+    //       },
+    //     ],
+    //     mode: "payment",		 		
+    //     success_url: `${req.headers.origin}/client/success?status=success&payment_id=${doc_id}`,		
+    //     cancel_url: `${req.headers.origin}/client/failed?status=error&payment_id=${doc_id}`,  	
+    //     metadata: {
+    //       plan_id: plan_id, // Add your custom data here
+    //       client_id: client_id, // You can add more custom key-value pairs as needed
+    //       payment_id: doc_id,
+    //     },				
+    //   });
+
+
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Sample Item",
             },
-            quantity: 1,
+            unit_amount: price * 100,
           },
-        ],
-        mode: "payment",		 		
-        success_url: `${req.headers.origin}/client/success?status=success&payment_id=${doc_id}`,		
-        cancel_url: `${req.headers.origin}/client/failed?status=error&payment_id=${doc_id}`,  	
-        metadata: {
-          plan_id: plan_id, // Add your custom data here
-          client_id: client_id, // You can add more custom key-value pairs as needed
-          payment_id: doc_id,
-        },				
-      });
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `${req.headers.origin}/client/success?status=success&payment_id=${doc_id}`,
+      cancel_url: `${req.headers.origin}/client/failed?status=error&payment_id=${doc_id}`,
+      metadata: {
+        plan_id: plan_id,
+        client_id: client_id,
+        payment_id: doc_id,
+      },
+    });
 
       res.status(200).json({ sessionId: session.id });
     } catch (err) {
